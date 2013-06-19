e58cacdec3312252827ffda3268eab10ec3a67fc
Robert Picard <mail@robert.io>
2013-06-18 22:25:10 -0400

    Add [l+c] link to submissions
    Closes #9
    
    An event is passed from alter.js to main.js when we want to open the tabs. The
    listener in main.js opens them up for us.
    
    This commit also has a few unrelated cosmetic changes to the code and a
    preference description.

--- data/alter.js
+++ data/alter.js
@@ -58,6 +58,54 @@ function addYoursLinkToNav() {
 
 }
 
+function showLinkPlusComments() {
+    // Grab a list of the comments links to insert our HTML after
+    commentsLinks = $("td.subtext a[href^='item?id=']");
+
+    // The markup for the basic link
+    HTML = "<a href='#' class='alterhn-lpc'>[l+c]</a>";
+
+    // Add the [l+c] link to each submission
+    commentsLinks.each(function(i) {
+
+        // Do some tree traversal to grab the URL of the submitted link.
+        //
+        // They aren't connected with a span or anything, so it's kind of
+        // hackish. There's probably a better way, but this works for now.
+        linkURL = $(this) // a
+            .parent() // td
+            .parent() // tr
+            .prev() // tr
+            .find("td.title a[href^='http']") // a
+            .attr("href"); // link URL
+
+        // We already have the comments anchor element, so we just need to grab
+        // the href element and concatenate it with the hostname and a forward
+        // slash.
+        commentsURL = window.location.host + "/" + $(this).attr("href");
+
+        // Create a jQuery object to make adding the data attributes easier
+        tmpLink = $(HTML);
+
+        // Store the links in data attributes for the click function
+        tmpLink.attr("data-alterhn-linkurl", linkURL);
+        tmpLink.attr("data-alterhn-commentsurl", commentsURL);
+
+        // Insert a space followed by the [l+c] link after the comments link
+        $(this).after(" ", tmpLink);
+    });
+
+    // This is the function that opens the tabs when the [l+c] link is clicked.
+    //
+    // We can't just use plain old JS because there's no way to open tabs like
+    // that. So, we send an event to the listener in main.js and let that open
+    // the tabs for us. Hooray for addons!
+    $(".alterhn-lpc").click(function() {
+        self.port.emit("openTab", $(this).attr("data-alterhn-commentsurl"));
+        self.port.emit("openTab", $(this).attr("data-alterhn-linkurl"));
+    });
+}
+
 // -- Make alterations --------------------------------------------------------
 
 self.port.on("gotPrefs", function(prefs) {
@@ -76,6 +124,10 @@ self.port.on("gotPrefs", function(prefs) {
 
     if (prefs.addYoursLinkToNav) {
         addYoursLinkToNav();
+    }
+
+    if (prefs.showLinkPlusComments) {
+        showLinkPlusComments();
     }
 });
 
